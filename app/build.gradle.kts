8e3e497bfa2bc98a05ba08c54810b99856698a4b
leon.wagner <leon.wagner@htl-braunau.at>
2023-10-03 15:34:11 +0200

    Seitenfenster und Zusatzdateien

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.htlbrml.a18"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.htlbrml.a18"
@@ -32,6 +32,8 @@ android {
 }
 
 dependencies {
+    implementation ("com.google.android.material:material:1.3.0-alpha03")
+    implementation ("com.google.android.material:material:1.4.0")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- app/src/main/java/com/htlbrml/a18/MainActivity.java
+++ app/src/main/java/com/htlbrml/a18/MainActivity.java
@@ -1,20 +1,58 @@
 package com.htlbrml.a18;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.MenuItem;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        // to make the Navigation drawer icon always appear on the action bar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        actionBarDrawerToggle.syncState();
     }
 
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-
+    // Add this method to handle the navigation drawer toggle state
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
 }
